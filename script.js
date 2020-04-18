0a8c921d4e3bc1d8496f5af0128b4e7524f3804c
Kat Yeary <katyeary@gmail.com>
2020-04-17 21:32:59 -0700

    script.js

--- script.js
+++ script.js
@@ -1,59 +1,51 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+var generateBtn = document.getElementbyId("#generate").onclick = writePassword();
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //sets the password length
+  var pwdLength = prompt("How long should your password be?\ Pick a number between 8 and 128");
+  var password = generatePassword(pwdLength);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-function generatePassword() {
-  const pwdLength = prompt("To set the length of your password, choose any number between 8 and 128:");
-  const values = [];
+//Generates password within the parameters of the selected length
+function generatePassword(length) {
 
-      if(confirm("lowercase")) {
-          values.push("abcdefghijklmnopqrstuvwxyz");
-      }
-      if(confirm("uppercase")) {
-          values.push("ABCDEFGHIJKLMNPQRSTUVWXYZ");
-      }
-      if(confirm("numbers")) {
-          values.push("1234567890");
-      }
-      if(confirm("characters")) {
-          values.push("!@#$%^&*()+-./:;<={}|~?");
-      }
-  let pword = "";
-  for(var i = 0; i <= pwdLength; i++) {
-      pword = pword + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
-      }
-} 
+  alert("In the following prompts, you'll be asked\ select what kind of characters to include.\ Click 'OK' for yes and 'Cancel' for no.");
 
-console.log(generatePassword());
+  //Creates an empty array and add strings of selected characters as they are confirmed.
+  var str = [];
+  if (confirm("lowercase")) {
+    str.push("abcdefghijklmnopqrstuvwxyz");
+  }
+  if (confirm("uppercase")) {
+    str.push("ABCDEFGHIJKLMNPQRSTUVWXYZ");
+  }
+  if (confirm("numbers")) {
+    str.push("1234567890");
+  }
+  if (confirm("special characters")) {
+    str.push("!@#$%^&*()+-./:;<={}|~?");
+  }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  //Concatenates these separate strings into a single array. 
+
+  var characters = str.join('');
+
+  //Computer selects random characters and pushes them into an empty array. This is the password.
+  var result = '';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 
 
 
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
 
 
