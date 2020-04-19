180a40181123bbdce52c9e631eab30ddd6e42e40
Kat Yeary <katyeary@gmail.com>
2020-04-18 18:57:38 -0700

    script.js

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
 //create event listener to trigger the password generator
 document.getElementbyId("#generate").onclick = writePassword();
 
-
 // Writes password to the #password input
 function writePassword() {
-	//sets the password length and 
-	var pwdLength = prompt("How long should your password be?\ Pick a number between 8 and 128");
-  var password = generatePassword(pwdLength);
+	var password = generatePassword();
 	var passwordText = document.querySelector("#password");
 	passwordText.value = password;
 }
 
-//Generates password within the parameters of the selected length
-function generatePassword(length) {
 
-	alert("In the following prompts, you'll be asked\ to select what kind of characters to include.\ Click 'OK' for yes and 'Cancel' for no.");
+function generatePassword() {
+	var pwdLength = prompt("How long should your password be?\ Pick a number between 8 and 128");
+	if (pwdLength < 8 || pwdLength > 128) {
+		alert("ERROR. Must select a number between 8 and 128. Page will reload.");
+    return location.reload();
+	} else {
+		alert("In the following prompts, you'll be asked" +
+			" to select what kind of characters to include." +
+			" Click 'OK' for yes and 'Cancel' for no.");
+	}
+ 
 
-	//Creates an empty array and adds strings of selected characters as they are confirmed.
-  var str = [];
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
-  var numbers = "1234567890";
-  var specialCharacters = "!@#$%^&*()+-./:;<={}|~?";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specialCharacters = "!@#$%^&*()+-./:;<={}|~?";
+var str = [];
 
-    if (confirm("lowercase")) {
-      str.push(lowercase);
-    }
-    if (confirm("uppercase")) {
-      str.push(uppercase);
-    }
-    if (confirm("numbers")) {
-      str.push(numbers);
-    }
-    if (confirm("special characters")) {
-      str.push(specialCharacters); 
-    }
-  
-  //Concatenates these separate strings into a single array. 
-  var characters = str.join('');
 
-  //Computer selects random characters, using the password length as a parameter, 
- //and returns them as a string. This is the password.
-	var result = '';
+	if (confirm("lowercase")) {
+		str.push(lowercase);
+	}
+	if (confirm("uppercase")) {
+		str.push(uppercase);
+	}
+	if (confirm("numbers")) {
+		str.push(numbers);
+	}
+	if (confirm("special characters")) {
+		str.push(specialCharacters);
+	}
+
+var characters = str.join('');
+if (characters === ("")) {
+	alert("ERROR. You did not select any character types. Page will Reload.");
+	return location.reload();
+} else {
 	var charactersLength = characters.length;
-	for (var i = 0; i < length; i++) {
+	//Computer selects random characters, using the password length as a parameter, 
+	//and returns them as a string. This is the password.
+	var result = '';
+	for (var i = 0; i < pwdLength; i++) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
 	}
 	return result;
+  }
 }
 
-/* function error {
-  alert("Error: you must select both a password length between 8-128 / and at least one character type");
-  location.reload(forceGet);
-}
- */
-
-
-
-
- 
